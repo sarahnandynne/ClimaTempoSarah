9ff3ee3a76ec152b2b4240271be84c8de6dedeae
SarahNandynne <miguelsarah051@gmail.com>
2024-04-16 11:51:17 -0300

    dia 16/04

---
+++ ClimaTempoSarah/Results.cs
@@ -0,0 +1,21 @@
+using System.Collections.Specialized;
+
+namespace ClimaTempoSarah;
+
+public class Results
+{
+    public int tempo {get; set;}
+    public string description {get; set;}
+    public string condition_code {get; set;}
+    public string img_id {get; set;}
+    public string city {get; set;}
+    public double rain {get; set;}
+    public int humidity {get; set;}
+    public string sunrise {get; set;}
+    public string sunset {get; set;}
+    public string wind_speedy {get; set;}
+    public int wind_direction {get; set;}
+    public string moon_phase {get; set;}
+    public string currently {get; set;}
+    
+}
