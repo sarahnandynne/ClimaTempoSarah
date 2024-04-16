5e5ecba2fc257aa959e3d7ad09390c20512929ec
SarahNandynne <miguelsarah051@gmail.com>
2024-04-16 10:47:36 -0300

    dia 16/04

---
+++ ClimaTempoSarah/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ClimaTempoSarah;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ClimaTempoSarah/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ClimaTempoSarah;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
